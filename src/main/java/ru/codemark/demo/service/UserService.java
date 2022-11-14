59a0a552d03ff596d4355d758289088b950f48b3
Roman <romabukhtiarov@gmail.com>
2022-11-14 08:48:51 +0300

    Fix issue when user roles can be set to null, if during update it was not passed

--- src/main/java/ru/codemark/demo/service/UserService.java
+++ src/main/java/ru/codemark/demo/service/UserService.java
@@ -3,11 +3,13 @@ package ru.codemark.demo.service;
 import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.codemark.demo.entity.Role;
 import ru.codemark.demo.entity.User;
 import ru.codemark.demo.repository.UserRepository;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class UserService {
@@ -27,7 +29,21 @@ public class UserService {
         return userByLogin;
     }
 
+    @Transactional
     public User saveOrUpdateUser(User user) {
+        User userByLogin = userRepository.findByLogin(user.getLogin());
+        if (userByLogin != null) {
+            //this field will not be null because it was checked earlier
+            userByLogin.setPassword(user.getPassword());
+            userByLogin.setName(user.getName());
+
+            Hibernate.initialize(userByLogin.getUserRoles());
+            Set<Role> userRoles = user.getUserRoles();
+            if (userRoles != null && userRoles.size() > 0) {
+                userByLogin.setUserRoles(userRoles);
+            }
+            user = userByLogin;
+        }
         User savedUser = userRepository.save(user);
         Hibernate.initialize(savedUser.getUserRoles());
         return savedUser;
@@ -37,4 +53,5 @@ public class UserService {
     public Integer deleteUserByLogin(String login) {
         return userRepository.deleteByLogin(login);
     }
+
 }
